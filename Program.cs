d77465c5cd37eede452b37f74bf4f063aad41625
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-04 23:58:46 -0300

    A resolução do exercício foi feita

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace etecab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
